e532eec2b1c9561cae17224618d8292b22247c3d
jackoy19 <jackneildelasalas@gmail.com>
2021-12-14 11:18:59 +0800

    Added while and switch to the main

--- main.c
+++ main.c
@@ -2,10 +2,33 @@
 #include <stdlib.h>
 
 int main(){
-	printf("MAIN MENU\n");
-	printf("[A] Add Inventory Item\n");
-	printf("[B] Update Inventory Item\n");
-	printf("[C] View Inventory List\n");
-	printf("[D] Search Inventory Item\n");
-	printf("[X] Exit Program\n");
+	
+	char choice = ' ';
+	
+	while(choice != 'X'){
+		printf("MAIN MENU\n");
+		printf("[A] Add Inventory Item\n");
+		printf("[B] Update Inventory Item\n");
+		printf("[C] View Inventory List\n");
+		printf("[D] Search Inventory Item\n");
+		printf("[X] Exit Program\n");
+ 		
+ 		switch(choice){
+ 			case 'A':
+ 				break;
+ 			case 'B':
+ 				break;
+ 			case 'C':
+ 				break;
+ 			case 'D':
+ 				break;
+ 			case 'X': printf("BYE");
+			 	break;
+			default:  printf("Please input valid choice!");
+ 				break;
+		}
+		scanf("%c", toupper(choice));
+		printf("%c", choice);   
+
+	}
 }
