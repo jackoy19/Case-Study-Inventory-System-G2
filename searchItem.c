ee8d7d71d9cea9d9a345c49ac41157e9e46c3355
Berns <ccbernardo21@gmail.com>
2021-12-14 20:27:51 +0800

    Working searchItem function and display details

--- searchItem.c
+++ searchItem.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "funcExist.c"
+#include "getLine.c"
 
 void searchItem()
 {
     int Item_ID;
     printf("Input Item ID: ");
     scanf("%d", &Item_ID);
-    if (!(Item_ID >= 11101 && Item_ID <= 69999))
-    {
+    if (!(Item_ID >= 11101 && Item_ID <= 69999)) {
         printf("Item ID is invalid!\n");
         return;
     }
-    if (funcExist(&Item_ID)) {
-        printf("ITEM FOUND: proceed with something\n");
+    int lineNum = funcExist(&Item_ID);
+    if (lineNum > 0) {
+        getLine(&lineNum);
     } else {
         printf("Item ID does not exist!\n");
     }
