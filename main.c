dfb0b0200f93a14257252a2a53064b7bf0ad0523
jackoy19 <jackneildelasalas@gmail.com>
2021-12-14 11:01:20 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+	printf("MAIN MENU\n");
+	printf("[A] Add Inventory Item\n");
+	printf("[B] Update Inventory Item\n");
+	printf("[C] View Inventory List\n");
+	printf("[D] Search Inventory Item\n");
+	printf("[X] Exit Program\n");
+}
