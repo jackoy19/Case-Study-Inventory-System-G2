1db6a09b15b50f6a68c3504ada2a289123af3467
jackoy19 <jackneildelasalas@gmail.com>
2021-12-15 22:52:08 +0800

    Update View inventory list.c

--- View inventory list.c
+++ View inventory list.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
 
 int main() {
-	FILE *the_file = fopen("Case Study CSV.csv", "r");
+	FILE *the_file = fopen("Inventory_ST_NoBOM.csv", "r");
 	if (the_file == NULL){
 		perror("unable to open the file.");
 		exit(1);
@@ -12,11 +16,11 @@ int main() {
 	{
 		char *token;
 		
-		token = strtok(line, ",");
+		token = strtok(line, "\",\"");
 		
 		while(token!= NULL) {
 			printf("%s", token);
-			token = strtok(NULL, ",");
+			token = strtok(NULL, "\",\"");
 		}
 		printf("\n");
 	}
