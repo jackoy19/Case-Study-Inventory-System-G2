46f5da6200a2fd7a412fb3ed14a1e05942a7aefd
Berns <ccbernardo21@gmail.com>
2021-12-16 13:58:35 +0800

    error if empty

--- EXPI_viewInvList.c
+++ EXPI_viewInvList.c
@@ -2,39 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-void EXPI_viewInvList()
-{
-    int lineOnFile = 1;
-    FILE *fptr = fopen("Inventory_ST_NoQuote_NoBOM.csv", "r");
-    char line[255];
-    int i = 0;
-    char details[5][120];
-
-    //header
-    printf("%-12s %-35s %-15s %-15s %-s\n", "Item ID", "Description", "Quantity", "Expiry Date", "Price");
-    //Reading of File
-    
-    while (!feof(fptr)) // loop until end of the file
-    {
-        
-        fgets(line, sizeof(line), fptr);
-        
-        char *lineptr = strtok(line, ","); // Split the string by using comma delimeter
-        
-        for (; i < 5; i++)
-        {
-            strcpy(details[i], lineptr); //copy the each values on the details array
-            lineptr = strtok(NULL, ",");
-        }
-       
-        printf("%-12s %-35s %-15s %-15s %s", details[0],details[1],details[2],details[3],details[4]);
-        i=0;
+int main(void) {
+    FILE *fptr = fopen("TESTinventory.csv", "r");
+    int size;
+    fseek (fptr, 0, SEEK_END);
+    size = ftell(fptr);
+    if (size == 0) {
+        printf("file is empty\n");
     }
-    printf("\n");
-    fclose(fptr);
-}
 
-// int main(void) {
-//     EXPI_viewInvList();
-//     return 0;
-// }
+    fclose(fptr);
+
+    return 0;
+}
