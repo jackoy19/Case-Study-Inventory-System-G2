0c867cdb7706562d1d32748316be44a43b51fb02
Berns <ccbernardo21@gmail.com>
2021-12-16 10:16:21 +0800

    add searchItemUNIT.c for test

---
+++ searchItemUNIT.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funcExist.c"
+#include "getLine.c"
+
+void searchItem()
+{
+    int Item_ID;
+    printf("Input Item ID: ");
+    scanf("%d", &Item_ID);
+    if (!(Item_ID >= 11101 && Item_ID <= 69999)) {
+        printf("Item ID is invalid!\n");
+        return;
+    }
+    int lineNum = funcExist(&Item_ID);
+    if (lineNum > 0) {
+        getLine(&lineNum);
+    } else {
+        printf("Item ID does not exist!\n");
+    }
+}
+
+int main(void)
+{
+    searchItem();
+    return 0;
+}
