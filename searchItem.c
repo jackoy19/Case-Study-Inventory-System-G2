6117034f94c89c204c283e46b79845a0f0908c7a
Berns <ccbernardo21@gmail.com>
2021-12-17 11:29:20 +0800

    fix for test case 38

--- searchItem.c
+++ searchItem.c
@@ -1,13 +1,20 @@
 void searchItem()
 {
     int Item_ID;
-    char input[6];
+    char input[7];
 
     fflush(stdin);
     
     printf("Input Item ID: ");
     fgets(input,7,stdin);
 
+    int len = strcspn(input, "\r\n");
+    //check if number is being padded with invalid char
+    if(len > 5) {
+        printf("\nItem ID is invalid!\n\n");
+        return;
+    }
+    //convert valid Item_ID string format input as int
     Item_ID = atoi(input);
 
     if (!(Item_ID >= 11101 && Item_ID <= 69999)) {
